353fd390975cbe22c28243e3e074b5c95892feb8
pollynsonia <160737847+pollynsonia@users.noreply.github.com>
2024-03-06 13:54:46 +0000

    codigos aula 02 pratica01

---
+++ aulas/aula02/modificadores.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  signed char caracter_com_sinal; // 1 byte -128 a 127
+  unsigned char caracter_sem_sinal; // 1 byte 0 a 255
+
+  signed int inteiro_com_sinal; // 4 bytes -2bt a 2bt
+
+  unsigned int inteiro_sem_sinal; // 4 bytes 0 a 4bt
+  
+  short int inteiro_curto; // 2 bytes -32mil a 32mil
+  unsigned int inteiro_curto_sem_sinal; // 2 bytes 0 a 32mil
+  
+  long int inteiro_longo; // 8 bytes -9hexa a 9hexa
+  unsigned long  int inteiro_longo_sem_sinal; // 8 bytes 0 a 9hexa 
+
+  long double duplo_longo; // 16 bytes -1.2e+4932 a 1.2e+4932
+
+
+
+
+
+
+
+
+
+
+  
+  return 0;
+}
---
+++ aulas/aula02/tipos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // "a", '1', '\n'
+  int inteiro; // -15, =15, 0
+float decimal6casas; // -15.000000, =15.000000
+  double decimal15casas // -15e=15, 15e+15
+  void sem valor; 
+  // string nao existe no C, "uma frase qualquer"
+  return 0;
+  
